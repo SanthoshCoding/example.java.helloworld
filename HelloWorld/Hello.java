7fc1f6023ffd938bce28d7d599cb4310b1af3910
SanthoshCoding <santhosh.nagendra64@gmail.com>
2021-04-19 23:15:24 +0530

    Changes version 2

--- HelloWorld/Hello.java
+++ HelloWorld/Hello.java
@@ -1,5 +1,5 @@
 class Hello{  
 	public static void main(String[] args){  
-		System.out.println("This is java app \n by using Java");  
+		System.out.println("This is java app \n by using Java 2");  
 	}  
 }  
